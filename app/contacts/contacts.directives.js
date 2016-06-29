1da234b2802d57b0cddcbd0631dfd7f636eb2cbe
aZerato <azerato.graphe@gmail.com>
2016-06-29 11:12:42 +0200

    not perfect but some directives samples

--- app/contacts/contacts.directives.js
+++ app/contacts/contacts.directives.js
@@ -17,4 +17,37 @@
 	 */
 	contactsModule.directive('myName', contactsFormDirective1);
 
+	var contactsFormDirective2 = function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'app/contacts/myName.html'
+		}
+	};
+	contactsModule.directive('myNamebis', contactsFormDirective2);
+
+	/*
+	 * Directives own scope.
+	 */
+	var contactsFormDirective3 = function() {
+		return {
+			restrict: 'E',
+			scope: {
+				name: '=titi'
+			},
+			templateUrl: 'app/contacts/myName.html'
+		}
+	};
+	contactsModule.directive('myNamebisbis', contactsFormDirective3);
+
+	var contactsFormDirective4 = function() {
+		return {
+			restrict: 'E',
+			scope: {
+				param: '@titi'
+			},
+			templateUrl: 'app/contacts/myNamebis.html'
+		}
+	};
+	contactsModule.directive('myNamebisbisbis', contactsFormDirective4);
+
 })(window.angular);
