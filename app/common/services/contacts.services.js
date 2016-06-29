e9c7d9067ee6811af23473d7718e21fe4cc698b4
aZerato <azerato.graphe@gmail.com>
2016-06-29 12:39:51 +0200

    add some comments

--- app/common/services/contacts.services.js
+++ app/common/services/contacts.services.js
@@ -1,8 +1,14 @@
 (function (angular) {
 	'use strict';
 
+	/*
+	 * Get the contacts module.
+	 */
 	var contactsModule = angular.module('contactsModule');
 
+	/*
+	 * Creation an instance your Contacts service.
+	 */
 	var contactsService = function($http) {
 		// you can use $scope but you need to inject then or this.
 		var self = this;
@@ -12,8 +18,14 @@
 		};
 	};
 
+	/*
+	 * Inject depencencies to your service.
+	 */
 	contactsService.$inject = ['$http'];
 
+	/*
+     * Inject your new service to module.
+	 */
 	contactsModule.service('contactsService', contactsService);
 
 })(window.angular);
