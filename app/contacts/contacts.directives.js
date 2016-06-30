3e626f37169e91b69406845d3ec9bd8b2f4d8bed
aZerato <azerato.graphe@gmail.com>
2016-06-30 18:43:23 +0200

    add some comments about directive

--- app/contacts/contacts.directives.js
+++ app/contacts/contacts.directives.js
@@ -12,6 +12,7 @@
 			templateUrl: 'app/contacts/contacts.list.html'
 		}
 	};
+	// look in contacts.html.
 	contactsModule.directive('contactsList', generateContactsListDirective);
 
 	var generateContactFormDirective = function() {
@@ -20,6 +21,7 @@
 			templateUrl: 'app/contacts/contacts.form.html'
 		}
 	};
+	// look in contacts.html.
 	contactsModule.directive('contactsForm', generateContactFormDirective);
 
 })(window.angular);
